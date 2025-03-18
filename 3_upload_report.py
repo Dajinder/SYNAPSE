867c2f70703324a7cf64c234c02e4538c24a56b6
Dajinder Singh <43824563+Dajinder@users.noreply.github.com>
2025-03-17 20:36:05 -0400

    Update 3_upload_report.py
    Addition of toggle on top right corner to switch themes

--- 3_upload_report.py
+++ 3_upload_report.py
@@ -1,12 +1,25 @@
 # pages/3_Upload_Report.py
 import streamlit as st
-from utils import extract_text_from_pdf, extract_text_from_image, convert_pdf_to_images
+from utils import extract_text_from_pdf, extract_text_from_image, convert_pdf_to_images, apply_theme
 import io
 import datetime
 from PIL import Image
 
 # Set page config
 st.set_page_config(page_title="Upload Report", page_icon="📤")
+
+ Apply the current theme
+apply_theme()
+
+
+# Top right theme toggle
+col1, col2 = st.columns([4, 1])
+with col2:
+    dark_mode = st.toggle("Dark Mode", value=(st.session_state.theme == "Dark"))
+    new_theme = "Dark" if dark_mode else "Light"
+    if new_theme != st.session_state.theme:
+        st.session_state.theme = new_theme
+        st.rerun()
 
 st.logo(image="logo_6.png", size="large")
 
